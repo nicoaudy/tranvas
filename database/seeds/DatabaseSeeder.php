17769a12567f5f4ad876eb98800210725e1c62c2
nicoaudy <nicoaudy1994@gmail.com>
2017-09-04 22:17:38 +0700

    auth scaffold and added seeder and migrations for users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use App\Modules\Event\Event;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name'      => 'NicoAudy',
+            'email'     => 'nico@tranvas.com',
+            'password'  => bcrypt('secret'),
+            'is_active' => 1
+        ]);
+
+        factory(Event::class, 20)->create();
         // $this->call(UsersTableSeeder::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('/', function () {
 
 Route::get('events', 'Event\EventController@index')->name('events');
 Route::get('events/view/{event}', 'Event\EventController@view')->name('event.view');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
