d10533b8a4a0cb6617054d6de4a1697851b43c5f
nicoaudy <nicoaudy1994@gmail.com>
2017-09-04 14:17:19 +0700

    fix event factory and try to migrate

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -10,11 +10,11 @@ $factory->define(Event::class, function (Faker $faker) {
     $end_date   = $start_date->copy()->addDays($faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 
     return [
-        'title'         => $faker->sentace(5),
+        'title'         => $faker->sentence(5),
         'description'   => $faker->paragraph(5),
         'address'       => $faker->address,
         'lat'           => $faker->latitude,
-        'long'          => $faker->longtitude,
+        'long'          => $faker->longitude,
         'start_date'    => $start_date->format('Y-m-d'),
         'end_date'      => $end_date->format('Y-m-d'),
         'user_id'       => factory(User::class)->create()->id,
