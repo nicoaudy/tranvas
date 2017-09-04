fc97aedd608906df3b8db5103517e4aa07ca06dc
nicoaudy <nicoaudy1994@gmail.com>
2017-09-04 14:12:53 +0700

    added eventfactory

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Carbon\Carbon;
+use App\Modules\Event\Event;
+use Faker\Generator as Faker;
+
+$factory->define(Event::class, function (Faker $faker) {
+    $start_date = Carbon::now()->addDays($faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+    $end_date   = $start_date->copy()->addDays($faker->randomElement([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+    return [
+        'title'         => $faker->sentace(5),
+        'description'   => $faker->paragraph(5),
+        'address'       => $faker->address,
+        'lat'           => $faker->latitude,
+        'long'          => $faker->longtitude,
+        'start_date'    => $start_date->format('Y-m-d'),
+        'end_date'      => $end_date->format('Y-m-d'),
+        'user_id'       => factory(User::class)->create()->id,
+    ];
+});
