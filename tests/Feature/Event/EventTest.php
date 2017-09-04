372c115d89a1823774c38e2f75e6f552aa90f678
nicoaudy <nicoaudy1994@gmail.com>
2017-09-04 17:14:04 +0700

    first test for event

--- tests/Feature/Event/EventTest.php
+++ tests/Feature/Event/EventTest.php
@@ -3,8 +3,21 @@
 namespace Tests\Feature\Event;
 
 use Tests\TestCase;
+use App\Modules\Event\Event;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class EventTest extends TestCase
 {
+    use DatabaseMigrations;
+
+    /** @test */
+    public function a_user_can_see_list_of_events()
+    {
+        $event = factory(Event::class)->create();
+
+        $this->get(route('events'))->assertStatus(200)
+        ->assertSeeText($event->title)
+        ->assertSeeText($event->description);
+    }
 }
