7ddd623f41df8562efc584d2588f0a050386eaf3
nicoaudy <nicoaudy1994@gmail.com>
2017-09-05 22:42:20 +0700

    create class abstract for repository

---
+++ app/Repositories/AbstractRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\AbstractInterface;
+
+class AbstractRepository implements AbstractInterface
+{
+    public function getById($id)
+    {
+        return $this->model->where('id', $id)->first();
+    }
+
+    public function getAll($limit = 10)
+    {
+        return $this->model->select()->orderBy('created_at', 'desc')->paginate($limit);
+    }
+
+    public function getAllNoLimit()
+    {
+        return $this->model->select()->orderBy('created_at', 'desc')->get();
+    }
+
+    public function create(array $attributes)
+    {
+        $data = $this->model->create($attributes);
+        return $data;
+    }
+
+    public function update(array $attributes, $id, $getDataBack = false)
+    {
+        $update = $this->model->where('id', $id)->update($attributes);
+        if ($getDataBack) {
+            $update = $this->getById($id);
+        }
+        return $update;
+    }
+
+    public function remove($id)
+    {
+        return $this->model->where('id', $id)->delete();
+    }
+}
