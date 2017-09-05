4515d8030773dd11ba63d08ab6241881fd0028c3
johndoe <nicoaudy1994@gmail.com>
2017-09-05 14:00:24 +0700

    installing vue2 google maps

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -3,17 +3,20 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Tranvas</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     @yield('header-style')
 </head>
 <body>
     @include('partials.menu')
-    <div class="container">
-        @yield('content')
+    <div class="container" id="app">
+        <div class="container-content">
+            @yield('content')
+        </div>
     </div>
 
-    <script src="js/app.js"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
     @stack('footer-script')
 </body>
 </html>
