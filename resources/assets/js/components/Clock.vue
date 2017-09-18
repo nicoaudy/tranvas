6bb514a83170845912f35c93767d96c8bbe738f5
nicoaudy <nicoaudy1994@gmail.com>
2017-09-18 22:07:11 +0700

    add clock component to dashboard

---
+++ resources/assets/js/components/Clock.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="clock-wrapper">
+        <div class="month-year">
+            {{ yearMonthString }}
+        </div>
+        <div class="date-time">
+            {{ dateTimeString }}
+        </div>
+    </div>
+</template>
+
+<script>
+    import moment from 'moment'
+    export default {
+        data() {
+            return {
+                yearMonthString: '',
+                dateTimeString: '',
+            }
+        },
+        created() {
+            this.dateTimeString = moment().format('ddd hh:mm:ss A')
+            this.yearMonthString = moment().format('MMMM YYYY')
+
+            var vm = this
+            setInterval(function() {
+                vm.dateTimeString = moment().format('ddd hh:mm:ss A')
+            }, 1000)
+        }
+    }
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,19 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
                     You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Clock</div>
+
+                <div class="panel-body">
+                    <clock></clock>
                 </div>
             </div>
         </div>
